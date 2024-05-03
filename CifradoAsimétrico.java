3c0ace0add40755d89577a259b614a2849c1ac34
natalia-ricaurte <na.ricaurtep@uniandes.edu.co>
2024-05-03 09:13:48 -0500

    Add files via upload

---
+++ CifradoAsimétrico.java
@@ -0,0 +1,36 @@
+import java.security.Key;
+import javax.crypto.Cipher;
+
+public class CifradoAsimétrico {
+	
+	public static byte[] cifrar(Key llave, String algoritmo, String texto) {
+		byte[] textoCifrado;
+		
+		try {
+			Cipher cifrador = Cipher.getInstance(algoritmo);
+			byte[] textoClaro = texto.getBytes();
+			
+			cifrador.init(Cipher.ENCRYPT_MODE, llave);
+			textoCifrado = cifrador.doFinal(textoClaro);
+			
+			return textoCifrado;
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+			return null;
+		}
+	}
+	
+	public static byte[] descifrar(Key llave, String algoritmo, byte[] texto) {
+		byte[] textoClaro;
+		
+		try {
+			Cipher cifrador = Cipher.getInstance(algoritmo);
+			cifrador.init(Cipher.DECRYPT_MODE, llave);
+			textoClaro = cifrador.doFinal(texto);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+			return null;
+		}
+		return textoClaro;
+	}
+}
